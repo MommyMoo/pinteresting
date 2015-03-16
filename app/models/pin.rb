2e02203d0865d5da493bad264f69170142b30d13
Debra J. Mernsick <dmersnick@hotmail.com>
2015-03-15 21:30:26 -0400

    Added names to users, fixed sign in page missing boxes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,8 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } #when a picture is uploaded it will create two sizes, a med. pic and a thumbnail.
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
 
